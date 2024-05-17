3bdf77d34121f2a7be13929bc3a23a12ebe11233
mckeownish <j.mckeown124@gmail.com>
2024-05-17 16:04:24 -0400

    change of src folder naming

--- makeFile_multi.sh
+++ makeFile_multi.sh
@@ -2,30 +2,30 @@
 echo "================================================================================"
 echo "Building carbonara..."
 
-if ls finalSrc/.o 1> /dev/null 2>&1; then
-rm finalSrc/.o
+if ls src/.o 1> /dev/null 2>&1; then
+rm src/.o
 fi
 
-g++ -c -O3 -std=gnu++14 -o finalSrc/point.o  finalSrc/point.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/polyHelix.o finalSrc/polyHelix.cpp 
-g++ -c -O3 -std=gnu++14 -o finalSrc/randomMolGen.o finalSrc/randomMolGen.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/ktlMoleculeRandom.o finalSrc/ktlMoleculeRandom.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/experimentalData.o finalSrc/experimentalData.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/hydrationShellRandom.o finalSrc/hydrationShellRandom.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/skmt.o finalSrc/skmt.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/writheFP.o finalSrc/writheFP.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/moleculeFitAndState.o finalSrc/moleculeFitAndState.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/Logger.o finalSrc/Logger.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/helpers.o finalSrc/helpers.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/parameters.o finalSrc/parameters.cpp
+g++ -c -O3 -std=gnu++14 -o src/point.o  src/point.cpp
+g++ -c -O3 -std=gnu++14 -o src/polyHelix.o src/polyHelix.cpp
+g++ -c -O3 -std=gnu++14 -o src/randomMolGen.o src/randomMolGen.cpp
+g++ -c -O3 -std=gnu++14 -o src/ktlMoleculeRandom.o src/ktlMoleculeRandom.cpp
+g++ -c -O3 -std=gnu++14 -o src/experimentalData.o src/experimentalData.cpp
+g++ -c -O3 -std=gnu++14 -o src/hydrationShellRandom.o src/hydrationShellRandom.cpp
+g++ -c -O3 -std=gnu++14 -o src/skmt.o src/skmt.cpp
+g++ -c -O3 -std=gnu++14 -o src/writheFP.o src/writheFP.cpp
+g++ -c -O3 -std=gnu++14 -o src/moleculeFitAndState.o src/moleculeFitAndState.cpp
+g++ -c -O3 -std=gnu++14 -o src/Logger.o src/Logger.cpp
+g++ -c -O3 -std=gnu++14 -o src/helpers.o src/helpers.cpp
+g++ -c -O3 -std=gnu++14 -o src/parameters.o src/parameters.cpp
 
 
-g++ -c -O3 -std=gnu++14 -o finalSrc/main_multi.o finalSrc/main_multi.cpp
-g++ -O3 -std=gnu++14 -o predictMulti finalSrc/point.o finalSrc/polyHelix.o finalSrc/randomMolGen.o finalSrc/ktlMoleculeRandom.o finalSrc/experimentalData.o finalSrc/hydrationShellRandom.o finalSrc/skmt.o finalSrc/writheFP.o finalSrc/moleculeFitAndState.o finalSrc/main_multi.o finalSrc/Logger.o finalSrc/helpers.o finalSrc/parameters.o
+g++ -c -O3 -std=gnu++14 -o src/main_multi.o src/main_multi.cpp
+g++ -O3 -std=gnu++14 -o predictMulti src/point.o src/polyHelix.o src/randomMolGen.o src/ktlMoleculeRandom.o src/experimentalData.o src/hydrationShellRandom.o src/skmt.o src/writheFP.o src/moleculeFitAndState.o src/main_multi.o src/Logger.o src/helpers.o src/parameters.o
 
-g++ -c -O3 -std=gnu++14 -o finalSrc/Flexible_generator.o finalSrc/Flexible_generator.cpp
+g++ -c -O3 -std=gnu++14 -o src/Flexible_generator.o src/Flexible_generator.cpp
 
-g++ -O3 -std=gnu++14 -o generate_structure finalSrc/point.o finalSrc/polyHelix.o finalSrc/randomMolGen.o finalSrc/ktlMoleculeRandom.o finalSrc/Flexible_generator.o
+g++ -O3 -std=gnu++14 -o generate_structure src/point.o src/polyHelix.o src/randomMolGen.o src/ktlMoleculeRandom.o src/Flexible_generator.o
 
 echo "\n"
 echo "Built carbonara!"
--- makeFingerPrintComparison.sh
+++ makeFingerPrintComparison.sh
@@ -1,11 +1,11 @@
 #/bin/sh
 
-g++ -c -O3 -std=gnu++14 -o finalSrc/point.o  finalSrc/point.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/polyHelix.o finalSrc/polyHelix.cpp 
-g++ -c -O3 -std=gnu++14 -o finalSrc/randomMolGen.o finalSrc/randomMolGen.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/ktlMoleculeRandom.o finalSrc/ktlMoleculeRandom.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/skmt.o finalSrc/skmt.cpp
-g++ -c -O3 -std=gnu++14 -o finalSrc/writheFP.o finalSrc/writheFP.cpp	
-g++ -c -O3 -std=gnu++14 -o finalSrc/mainFileCompareFingerprints.o finalSrc/mainFileCompareFingerprints.cpp
+g++ -c -O3 -std=gnu++14 -o src/point.o  src/point.cpp
+g++ -c -O3 -std=gnu++14 -o src/polyHelix.o src/polyHelix.cpp
+g++ -c -O3 -std=gnu++14 -o src/randomMolGen.o src/randomMolGen.cpp
+g++ -c -O3 -std=gnu++14 -o src/ktlMoleculeRandom.o src/ktlMoleculeRandom.cpp
+g++ -c -O3 -std=gnu++14 -o src/skmt.o src/skmt.cpp
+g++ -c -O3 -std=gnu++14 -o src/writheFP.o src/writheFP.cpp
+g++ -c -O3 -std=gnu++14 -o src/mainFileCompareFingerprints.o src/mainFileCompareFingerprints.cpp
 
-g++ -O3 -std=gnu++14 -o compareFingerprints finalSrc/point.o finalSrc/polyHelix.o finalSrc/randomMolGen.o finalSrc/ktlMoleculeRandom.o finalSrc/skmt.o finalSrc/writheFP.o finalSrc/mainFileCompareFingerprints.o
+g++ -O3 -std=gnu++14 -o compareFingerprints src/point.o src/polyHelix.o src/randomMolGen.o src/ktlMoleculeRandom.o src/skmt.o src/writheFP.o src/mainFileCompareFingerprints.o
