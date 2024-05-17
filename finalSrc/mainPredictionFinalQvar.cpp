38fdf496427877638b5c561da63464bc1f709d34
mckeownish <j.mckeown124@gmail.com>
2024-05-17 08:04:16 -0400

    refactoring

--- finalSrc/mainPredictionFinalQvar.cpp
+++ finalSrc/mainPredictionFinalQvar.cpp
@@ -14,6 +14,9 @@
 
 using namespace std::chrono;
 
+// combination of all structures = moleculeStructures
+// each structure is 
+
 // note: this version showing funky behaviour with getFit()'s - not always consistent when recalled!
 
 /* --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- 
@@ -59,15 +62,15 @@ int main(int argc, const char* argv[]) {
    or some initial structure provided. Actually we need a half-half option */
 
   /* Initialise the molecule(s) vector */
-  std::vector<ktlMolecule> mol;
-  readInStructures(argv, mol, params);
+  std::vector<ktlMolecule> moleculeStructures;
+  readInStructures(argv, moleculeStructures, params);
 
   /* Determine which sections are being altered */
   std::vector<std::vector<int>> vary_sec_list_list;
   determineVaryingSections(argv, vary_sec_list_list);
 
   /* Read in any fixed distances constraints (contact predictions/sulfide bonds) */
-  readFixedDistancesConstraints(argv, mol);
+  readFixedDistancesConstraints(argv, moleculeStructures);
 
   /* Read in the permissible mixture list */
   readPermissibleMixtures(argv, params);
@@ -79,7 +82,7 @@ int main(int argc, const char* argv[]) {
   RandomGenerator rng;
 
   /* initialise the state of mol vector */
-  moleculeFitAndState molState(mol, params);
+  moleculeFitAndState molState(moleculeStructures, params);
 
   int improvementIndex = 0;
   // If we resume from previous run - argv[3] restart True/False
@@ -97,7 +100,7 @@ int main(int argc, const char* argv[]) {
   logger.logMetadata(argv[16], params);
 
   std::string scatterNameInitial = write_scatter(argv[12], improvementIndex, molState, ed, params.kmin, params.kmaxCurr, "initial");
-  std::string xyzNameInitial = write_molecules(argv[12], improvementIndex, mol, "initial");
+  std::string xyzNameInitial = write_molecules(argv[12], improvementIndex, moleculeStructures, "initial");
 
   // log starting point
   logger.logEntry(0, 0, overallFit.first, molState.getWrithePenalty(), molState.getOverlapPenalty(),
@@ -109,7 +112,7 @@ int main(int argc, const char* argv[]) {
 
   // numberOfChainsInEachStructure vector tells us how many chains are in each structure
   // e.g. for a monomer/dimer mixture numberOfChainsInEachStructure[0]=1, numberOfChainsInEachStructure[1]=2.
-  std::vector<int> numberOfChainsInEachStructure = findNumberSections(mol);
+  std::vector<int> numberOfChainsInEachStructure = findNumberSections(moleculeStructures);
 
   /* initialise the set of historical states - currently basic, but used to save previous fit stages */
   std::vector<moleculeFitAndState> molStateSet = makeHistoricalStateSet(molState, params);
@@ -131,10 +134,10 @@ int main(int argc, const char* argv[]) {
     // to become update function
     int historicFitIndex = rng.getChangeIndexProbability(fitStep, params);
     molState = molStateSet[historicFitIndex];
-    mol = molState.getMolecule();
+    moleculeStructures = molState.getMolecule();
     overallFit = molState.getFit();
 
-    for (int structureIndex = 0; structureIndex < mol.size(); structureIndex++) {
+    for (int structureIndex = 0; structureIndex < moleculeStructures.size(); structureIndex++) {
       int netIndex = 0;
 
       // loop over the sections of the given molecule (i.e. if its a monomer this loop is tivial, but not for a multimer
@@ -144,7 +147,7 @@ int main(int argc, const char* argv[]) {
         // Selected transformation option?
         if (params.affineTrans == true) {
 
-          ktlMolecule molCopyR = mol[structureIndex];
+          ktlMolecule molCopyR = moleculeStructures[structureIndex];
 
           double angle = rng.getRotAng();
           double theta = rng.getTheAng();
@@ -169,7 +172,7 @@ int main(int argc, const char* argv[]) {
             if (checkTransition(newOverallFit.first, overallFit.first, uProb, fitStep, params.noScatterFitSteps)) {
 
               improvementIndex++;
-              updateAndLog(improvementIndex, mol, molCopyR, molState, newMolState, overallFit, newOverallFit, logger, structureIndex, fitStep, ed, params);
+              updateAndLog(improvementIndex, moleculeStructures, molCopyR, molState, newMolState, overallFit, newOverallFit, logger, structureIndex, fitStep, ed, params);
 
               logger.consoleChange("fitImprove", params);
             }
@@ -179,14 +182,14 @@ int main(int argc, const char* argv[]) {
         // net index tells us how far we are through the whole moelcule
         // CHRIS - does this account for more than 1 previous chain in mol structure? 
         if (chainNumber > 1) {
-          netIndex = netIndex + mol[structureIndex].getSubsecSize(chainNumber - 1);
+          netIndex = netIndex + moleculeStructures[structureIndex].getSubsecSize(chainNumber - 1);
         }
 
         // not sure - Chris - all varying sections up for grabs?
         bool doAll = false;
 
         // Now loop over the secondary structures of the given unit or section
-        for (int secondarySectionIndex = 0; secondarySectionIndex < mol[structureIndex].getSubsecSize(chainNumber) - 1; secondarySectionIndex++) {
+        for (int secondarySectionIndex = 0; secondarySectionIndex < moleculeStructures[structureIndex].getSubsecSize(chainNumber) - 1; secondarySectionIndex++) {
 
           int totalIndex = netIndex + secondarySectionIndex;
 
@@ -194,8 +197,8 @@ int main(int argc, const char* argv[]) {
           if ((doAll == true) || (std::find(vary_sec_list_list[structureIndex].begin(), vary_sec_list_list[structureIndex].end(), totalIndex) != vary_sec_list_list[structureIndex].end())) {
 
             int indexCh = totalIndex - netIndex;
-            ktlMolecule newMol = mol[structureIndex];
-            bool cacaDist = modifyMolecule(newMol, mol[structureIndex], indexCh, chainNumber);
+            ktlMolecule newMol = moleculeStructures[structureIndex];
+            bool cacaDist = modifyMolecule(newMol, moleculeStructures[structureIndex], indexCh, chainNumber);
 
             if (cacaDist == false) {
 
@@ -210,7 +213,7 @@ int main(int argc, const char* argv[]) {
 
                 // Success! Add to the update index
                 improvementIndex++;
-                updateAndLog(improvementIndex, mol, newMol, molState, newmolState, overallFit, newOverallFit, logger, structureIndex, fitStep, ed, params);
+                updateAndLog(improvementIndex, moleculeStructures, newMol, molState, newmolState, overallFit, newOverallFit, logger, structureIndex, fitStep, ed, params);
                 logger.consoleChange("fitImprove", params);
 
               }
@@ -224,7 +227,7 @@ int main(int argc, const char* argv[]) {
 
     // Assign the new 'improved' molecule state to the historical tracker
     molStateSet[historicFitIndex] = molState;
-    molStateSet[historicFitIndex].updateMolecule(mol);
+    molStateSet[historicFitIndex].updateMolecule(moleculeStructures);
     sortVec(molStateSet);
 
     // Print out to terminal window
@@ -235,7 +238,7 @@ int main(int argc, const char* argv[]) {
 
   improvementIndex++;
 
-  std::string moleculeNameEnd = write_molecules(argv[12], improvementIndex, mol, "end");
+  std::string moleculeNameEnd = write_molecules(argv[12], improvementIndex, moleculeStructures, "end");
   std::string scatterNameEnd = write_scatter(argv[12], improvementIndex, molState, ed, params.kmin, params.kmaxCurr, "end");
 
   std::cout << "\n best overall mol name: " << moleculeNameEnd << "\n";
